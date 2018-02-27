7dd2c29b68936723547356a785d3489c0c2ea276
EvanWashkow <epwashkow@gmail.com>
2018-02-27 14:14:49 -0800

    Deactivate plugin if its dependency is not found

--- Dependencies/Dependency.php
+++ Dependencies/Dependency.php
@@ -1,6 +1,9 @@
 <?php
 namespace WordPress\Plugins\Dependencies;
 
+use WordPress\Plugins;
+use WordPress\Sites;
+
 /**
  * Defines a single plugin dependency, auto-activating it when the main plugin
  * is activated
@@ -11,16 +14,16 @@ final class Dependency
     /**
      * ID of the plugin being included as a dependency
      *
-     * @var string
+     * @var Plugins\Models\Plugin
      */
-    private $dependencyID;
+    private $dependency;
     
     /**
      * ID of the main plugin that requires the dependency
      *
-     * @var string
+     * @var Plugins\Models\Plugin
      */
-    private $pluginID;
+    private $plugin;
     
     
     /**
@@ -31,7 +34,25 @@ final class Dependency
      */
     public function __construct( string $pluginID, string $dependencyID )
     {
-        $this->pluginID     = $pluginID;
-        $this->dependencyID = $dependencyID;
+        // Set properties
+        $this->plugin     = Plugins::Get( $pluginID );
+        $this->dependency = Plugins::Get( $dependencyID );
+        
+        // Deactivate plugin if the dependency does not exist
+        if ( null === $this->dependency ) {
+            $this->deactivatePlugin();
+        }
+    }
+    
+    
+    /**
+     * Deactivate the plugin
+     *
+     * @return bool
+     */
+    private function deactivatePlugin()
+    {
+        $this->plugin->deactivate( Sites::ALL );
+        return $this->plugin->deactivate( Sites::CURRENT );
     }
 }
