98cba7d96f257853c2c81d8c45e98eaec12f19af
EvanWashkow <epwashkow@gmail.com>
2018-02-27 12:19:15 -0800

    Add custom plugin file header ID

--- Dependencies.php
+++ Dependencies.php
@@ -4,7 +4,27 @@ namespace WordPress\Plugins;
 /**
  * Defines a manager for WordPress plugin dependencies
  */
-class Dependencies
+final class Dependencies
 {
     
+    /**
+     * The plugin file's header key for listing dependencies
+     *
+     * @var string
+     */
+    const FILE_HEADER_ID = 'Dependencies';
+    
+    
+    /**
+     * Start the plugin dependency manager
+     */
+    public static function Initialize()
+    {
+        // Add custom plugin file header
+        add_filter( 'extra_plugin_headers', function( array $headers ) {
+            $headers[] = self::FILE_HEADER_ID;
+            return $headers;
+        });
+    }
 }
+Dependencies::Initialize();
