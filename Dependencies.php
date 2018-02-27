fe4a94ea9b46908ebb4a68b532d7cb0863075abb
EvanWashkow <epwashkow@gmail.com>
2018-02-27 12:18:38 -0800

    Fix local build

--- Dependencies.php
+++ Dependencies.php
@@ -1,12 +1,6 @@
 <?php
 namespace WordPress\Plugins;
 
-// Include vendor files if on a local build
-if ( file_exists( __DIR__ . '/vendor/autoload.php' )) {
-    require_once( __DIR__ . '/vendor/autoload.php' );
-    return;
-}
-
 /**
  * Defines a manager for WordPress plugin dependencies
  */
